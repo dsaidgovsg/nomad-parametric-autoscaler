ef9c81f5c9f0dc47cd0c011fe5ca19d6d12b1927
Sylvester Chin <sylvester.csw@gmail.com>
2019-03-14 16:31:49 +0800

    New feature: Monitoring resources (#3)
    * include GET status endpoint
    
    * tidy up
    
    * add endpoint documentation

--- app/router.go
+++ app/router.go
@@ -7,18 +7,21 @@ import (
 func NewRouter(ep *endpoints) *gin.Engine {
 	router := gin.Default()
 
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-
+	// Core endpoints
 	router.GET("/state", ep.GetPolicy)
+	router.GET("/status", ep.GetResourceStatus)
 	router.POST("/update", ep.UpdatePolicy)
 
 	// Helper endpoints
 	router.PUT("/pause", ep.PausePolicy)
 	router.PUT("/resume", ep.ResumePolicy)
+
+	// Healthcheck
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 
 	return router
 }
