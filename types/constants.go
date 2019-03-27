2f48c39c66fbc24e572129c9c8ccc932b13fc55a
slai11 <sylvester.csw@gmail.com>
2019-03-27 13:26:02 +0800

    Move all string constants into a separate file

---
+++ types/constants.go
@@ -0,0 +1,12 @@
+package types
+
+const CoreRatio = "CoreRatio"
+const OfficeHour = "OfficeHour"
+
+const ChangeTypeMultiply = "multiply"
+const ChangeTypeUntil = "until"
+
+const EnsembleConservative = "Conservative"
+const EnsembleAverage = "Average"
+
+var SubpolicyList = []string{CoreRatio, OfficeHour}
