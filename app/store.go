31894aa7ab0d3db50ffcd41bcd9a32d46cb9d2f6
slai11 <sylvester.csw@gmail.com>
2019-04-15 21:13:44 +0800

    Fix variable naming

--- app/store.go
+++ app/store.go
@@ -37,23 +37,23 @@ func dbConfig() map[string]string {
 	conf := make(map[string]string)
 	host, ok := os.LookupEnv(dbhost)
 	if !ok {
-		panic("DBHOST environment variable required but not set")
+		panic("POSTGRES_HOST environment variable required but not set")
 	}
 	port, ok := os.LookupEnv(dbport)
 	if !ok {
-		panic("DBPORT environment variable required but not set")
+		panic("POSTGRES_PORT environment variable required but not set")
 	}
 	user, ok := os.LookupEnv(dbuser)
 	if !ok {
-		panic("DBUSER environment variable required but not set")
+		panic("POSTGRES_USER environment variable required but not set")
 	}
 	password, ok := os.LookupEnv(dbpass)
 	if !ok {
-		panic("DBPASS environment variable required but not set")
+		panic("POSTGRES_PASSWORD environment variable required but not set")
 	}
 	name, ok := os.LookupEnv(dbname)
 	if !ok {
-		panic("DBNAME environment variable required but not set")
+		panic("POSTGRES_DB environment variable required but not set")
 	}
 	conf[dbhost] = host
 	conf[dbport] = port
