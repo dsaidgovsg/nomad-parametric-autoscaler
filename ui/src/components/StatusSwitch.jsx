fc528e0ddd6ef60eca39407975d19e5c2f70d409
slai11 <sylvester.csw@gmail.com>
2019-06-15 22:59:05 +0800

    Refactor StatusSwitch to use hooks

--- ui/src/components/StatusSwitch.jsx
+++ ui/src/components/StatusSwitch.jsx
@@ -1,43 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Switch from "@material-ui/core/Switch";
 import axios from "axios";
 
-// TODO convert to functional
-class StatusSwitch extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-  }
+const StatusSwitch = () => {
+  const [isRunning, setIsRunning] = useState(true);
 
-  handleChange(event) {
+  useEffect(() => {
+    const stateUrl = new URL(
+      "/state",
+      window.config.env.REACT_APP_NOPAS_ENDPOINT
+    );
+
+    axios.get(stateUrl)
+    .then(rsp => setIsRunning(rsp.data))
+    .catch(function (error) {
+      console.log(error);
+    })
+  }, []);
+
+  const handleChange = (event) => {
     if (event.target.checked) {
       const resumeUrl = new URL(
         "/state/resume",
         window.config.env.REACT_APP_NOPAS_ENDPOINT
       );
-      axios.put(resumeUrl);
+      axios.put(resumeUrl)
+      .then(rsp => setIsRunning(true))
+      .catch(err => {console.log(err)})
+
     } else {
       const pauseUrl = new URL(
         "/state/pause",
         window.config.env.REACT_APP_NOPAS_ENDPOINT
       );
-      axios.put(pauseUrl);
+      axios.put(pauseUrl)
+      .then(rsp => setIsRunning(false))
+      .catch(err => {console.log(err)})
     }
-
-    this.props.refreshState()
   }
 
-  render() {
-    return (
-      <FormControlLabel
-        control={
-          <Switch checked={this.props.isRunning} onChange={this.handleChange} />
-        }
-        label={this.props.isRunning ? "Running" : "Paused"}
-      />
-    );
-  }
+  return (
+    <FormControlLabel
+      control={
+        <Switch checked={isRunning} onChange={handleChange} />
+      }
+      label={isRunning ? "Running" : "Paused"}
+    />
+  );
 }
 
 export default StatusSwitch;
